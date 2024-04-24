44d6face0f78583f60dd057fa8b808386d6de88b
GrantBirki <grantbirki@github.com>
2024-04-24 11:38:57 -0700

    add `delete_deployment()` method

--- src/octokit/client/deployments.cr
+++ src/octokit/client/deployments.cr
@@ -64,6 +64,26 @@ module Octokit
       def create_deployment(repo, ref, **options)
         post("#{Repository.path(repo)}/deployments", options.merge(ref: ref))
       end
+
+      # Delete a Deployment
+      #
+      # If the repository only has one deployment, you can delete the deployment regardless of its status. If the repository has more than one deployment, you can only delete inactive deployments. This ensures that repositories with multiple deployments will always have an active deployment.
+      #
+      # To set a deployment as inactive, you must:
+      #
+      # Create a new deployment that is active so that the system has a record of the current state, then delete the previously active deployment.
+      # Mark the active deployment as inactive by adding any non-successful deployment status.
+      #
+      # **See Also:**
+      # - [https://developer.github.com/v3/repos/deployments/#delete-a-deployment](https://developer.github.com/v3/repos/deployments/#delete-a-deployment)
+      # **Examples:**
+      #
+      # ```
+      # Octokit.delete_deployment("monalisa/app", 123456)
+      # ```
+      def delete_deployment(repo, deployment_id, **options)
+        delete("#{Repository.path(repo)}/deployments/#{deployment_id}", options)
+      end
     end
   end
 end
