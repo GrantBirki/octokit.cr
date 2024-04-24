c36bf2d799caa7aa4ad810c1268fa3453c9c434f
GrantBirki <grantbirki@github.com>
2024-04-24 14:30:28 -0700

    complete deployment module

--- src/octokit/client/deployments.cr
+++ src/octokit/client/deployments.cr
@@ -1,5 +1,6 @@
 require "../models/repos"
 require "../models/repo_deployments"
+require "halite"
 
 module Octokit
   class Client
@@ -11,6 +12,9 @@ module Octokit
     module Deployments
       # :nodoc:
       alias Repository = Models::Repository
+
+      # :nodoc:
+      VALID_STATES = %w[error failure inactive in_progress queued pending success]
 
       # Fetch a single deployment for a repository
       #
@@ -24,7 +28,7 @@ module Octokit
       # ```
       # Octokit.deployment("monsalisa/app", 123456)
       # ```
-      def deployment(repo : String, deployment_id : Int32, **options)
+      def deployment(repo : String, deployment_id : Int64, **options)
         get("#{Repository.path(repo)}/deployments/#{deployment_id}", options)
       end
 
@@ -62,8 +66,38 @@ module Octokit
       # ```
       # Octokit.create_deployment("monalisa/app", "main")
       # ```
-      def create_deployment(repo : String, ref : String, **options)
-        post("#{Repository.path(repo)}/deployments", options.merge(ref: ref))
+      #
+      # ```
+      # Octokit.create_deployment("monalisa/app", "main", description: "Deploying main branch!")
+      # ```
+      def create_deployment(
+        repo : String, # in org/repo format (required)
+        ref : String,  # The ref to deploy. This can be a branch, tag, or SHA. (required)
+        task : String = "deploy",
+        auto_merge : Bool = true,
+        required_contexts : Array(String)? = nil,
+        payload : Hash(String, String) = {} of String => String,
+        environment : String = "production",
+        description : String = "",
+        transient_environment : Bool = false,
+        production_environment : Bool = true,
+        **options
+      )
+        options = options.merge({
+          json: {
+            ref:                    ref,
+            task:                   task,
+            auto_merge:             auto_merge,
+            required_contexts:      required_contexts,
+            payload:                payload,
+            environment:            environment,
+            description:            description,
+            transient_environment:  transient_environment,
+            production_environment: production_environment,
+          },
+        })
+
+        post("#{Repository.path(repo)}/deployments", options)
       end
 
       # Delete a Deployment
@@ -82,7 +116,7 @@ module Octokit
       # ```
       # Octokit.delete_deployment("monalisa/app", 123456)
       # ```
-      def delete_deployment(repo : String, deployment_id : Int32, **options)
+      def delete_deployment(repo : String, deployment_id : Int64, **options)
         delete("#{Repository.path(repo)}/deployments/#{deployment_id}", options)
       end
 
@@ -94,7 +128,7 @@ module Octokit
       # **Examples:**
       #
       # ```
-      # Octokit.deployment_statuses("https://api.github.com/repos/monalisa/app/deployments/123456")
+      # Octokit.deployment_statuses("monalisa/app", 1234567890)
       # ```
       #
       # You should use a paginated octokit instance to fetch all statuses:
@@ -104,23 +138,69 @@ module Octokit
       # client.auto_paginate = true
       # client.per_page = 100
       #
-      # data = client.deployment_statuses("https://api.github.com/repos/<org>/<repo>/deployments/<deployment_id>")
+      # data = client.deployment_statuses("monalisa/app", 1234567890)
       # puts data.records.to_pretty_json
       # ```
       #
       # Returns an array of deployment statuses
-      def deployment_statuses(deployment_url : String, **options) : Paginator(Octokit::Models::DeploymentStatus)
+      def deployment_statuses(repo : String, deployment_id : Int64, **options) : Paginator(Octokit::Models::DeploymentStatus)
         paginate(
           Octokit::Models::DeploymentStatus,
-          deployment_url + "/statuses",
+          "#{Repository.path(repo)}/deployments/#{deployment_id}" + "/statuses",
           start_page: options[:page]?,
           per_page: options[:per_page]?
         )
       end
 
       # alias for `deployment_statuses`
-      def list_deployment_statuses(deployment_url : String, **options) : Paginator(Octokit::Models::DeploymentStatus)
-        deployment_statuses(deployment_url, **options)
+      def list_deployment_statuses(repo : String, deployment_id : Int64, **options) : Paginator(Octokit::Models::DeploymentStatus)
+        deployment_statuses(repo, deployment_id, **options)
+      end
+
+      # Create a deployment status for a Deployment
+      #
+      # :param repo [String]: The repository to create a deployment status for in org/repo format (required)
+      # :param deployment_id [Integer]: The deployment id (required) 
+      # :param state: can be one of the following: error, failure, inactive, in_progress, queued, pending, success
+      # :param options [String]: :target_url The target URL to associate with this status. Default: ""
+      # :param options [String]: :description A short description of the status. Maximum length of 140 characters. Default: ""
+      # :return: A deployment status
+      #
+      # **See Also:**
+      # - [https://developer.github.com/v3/repos/deployments/#create-a-deployment-status](https://developer.github.com/v3/repos/deployments/#create-a-deployment-status)
+      #
+      # **Examples:**
+      #
+      # ```
+      # Octokit.create_deployment_status("monalisa/app", 1234567890, "success")
+      # ```
+      def create_deployment_status(
+        repo : String, # in org/repo format (required)
+        deployment_id : Int64, # The deployment id (required)
+        state : String,          # The state of the status (required)
+        target_url : String = "",
+        log_url : String = "",
+        description : String = "",
+        environment : String? = nil,
+        environment_url : String = "",
+        auto_inactive : Bool = true,
+        **options
+      )
+        raise "ArgumentError: state must be one of error, failure, inactive, in_progress, queued, pending, success" unless VALID_STATES.includes?(state)
+
+        options = options.merge({
+          json: {
+            state:           state.downcase,
+            target_url:      target_url,
+            log_url:         log_url,
+            description:     description,
+            environment:     environment,
+            environment_url: environment_url,
+            auto_inactive:   auto_inactive,
+          },
+        })
+
+        post("#{Repository.path(repo)}/deployments/#{deployment_id}" + "/statuses", options)
       end
     end
   end
